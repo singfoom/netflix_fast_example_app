eeea62dd13787b1f0cd5e213ea94c40113e9aa21
Colin Campbell <singfoom@gmail.com>
2018-04-18 16:16:30 -0500

    Perf spec cleanup

--- spec/object_serializer_performance_spec.rb
+++ spec/object_serializer_performance_spec.rb
@@ -8,15 +8,14 @@ describe FastJsonapi::ObjectSerializer, performance: true do
   after(:all) { GC.enable }
 
   context 'testing performance of serialization' do
+    let!(:created_books) { FactoryBot.create_list(:book, 1000) }
     context 'FastBookSerializer' do
 
       it 'creates a hash of 1000 records in less than 50 ms' do
-        created_books = FactoryBot.create_list(:book, 1000)
         expect { FastBookSerializer.new(created_books).serializable_hash }.to perform_under(100).ms
       end
 
       it 'serialize 1000 records to jsonapi in less than 60 ms' do
-        created_books = FactoryBot.create_list(:book, 1000)
         expect { FastBookSerializer.new(created_books).serialized_json }.to perform_under(150).ms
       end
 
@@ -24,18 +23,13 @@ describe FastJsonapi::ObjectSerializer, performance: true do
 
     context 'BookSerializer' do
       it 'creates a hash of 1000 records in less than 480 ms' do
-        created_books = FactoryBot.create_list(:book, 1000)
         expect { ActiveModelSerializers::SerializableResource.new(created_books).serializable_hash }.to perform_under(500).ms
       end
 
       it 'serialize 1000 records to jsonapi in less than 60 ms' do
-        created_books = FactoryBot.create_list(:book, 1000)
         expect { ActiveModelSerializers::SerializableResource.new(created_books,
                                                        :adapter => :json_api).to_json }.to perform_under(580).ms
       end
     end
-
-
   end
-
 end
